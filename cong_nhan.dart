d4977927db3d922adf08072f0f9561783c70e1ee
Bùi Tuấn Tú <buituantu20112003@gmail.com>
2024-08-26 17:18:59 +0700

    Update cong_nhan.dart

--- cong_nhan.dart
+++ cong_nhan.dart
@@ -4,15 +4,20 @@ import 'person.dart';
 
 class Personel extends Person{
   
-  String postion; //Vị trí công việc
+  List<String> positions = ["Truong phong", "Nhan vien"]; //Vị trí công việc
+  
+
+  String position; //Vị trí công việc
   String address; //Địa chỉ làm việc
   int salary; //Mức lương
 
-  Personel(this.postion, this.address, this.salary, String name, int age, String hometown, int year) : super(name, age, hometown, year);
+  Personel(this.position, this.address, this.salary, String name, int age, String hometown, int year) : super(name, age, hometown, year);
 
 
   @override
   String toString() {
-    return 'Họ và tên: $name, Tuổi: $age, Quê quán: $hometown, Năm sinh: $year, Vị trí công việc: $postion, Địa chỉ làm việc: $address, Mức lương: $salary';
+    return 'Họ và tên: $name, Tuổi: $age, Quê quán: $hometown, Năm sinh: $year, Vị trí công việc: $position, Địa chỉ làm việc: $address, Mức lương: $salary';
   }
+
+  static void sort(int Function(dynamic a, dynamic b) param0) {}
 }
